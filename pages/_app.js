b7d858215310a49e5234501ed405339d5b0f5fe7
Jordan Accinelli <jtaccinelli@gmail.com>
2022-01-31 23:31:24 +1000

    Removed tailwind, fixed packagejson

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import { UserProvider } from "@auth0/nextjs-auth0";
 import { GlobalStyle } from "components/styles/globals";
-import "tailwindcss/tailwind.css";
 
 function App({ Component, pageProps }) {
     return (
