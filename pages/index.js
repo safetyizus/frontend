1e48c6328c94ac86f18481d7aaca0b87c258fb05
Jordan Accinelli <jtaccinelli@gmail.com>
2022-02-05 16:11:58 +1000

    Moved Header file

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Header from "common/Header";
+import Header from "components/sections/Header";
 import withLayout from "hocs/withLayout";
 
 const Page = () => {
--- pages/invoices/[id]/edit.js
+++ pages/invoices/[id]/edit.js
@@ -1,4 +1,4 @@
-import Header from "components/common/Header";
+import Header from "components/sections/Header";
 import withLayout from "components/hocs/withLayout";
 
 const Page = ({ id }) => {
