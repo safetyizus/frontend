4209768979bffd7b31d42159d053e191631fd511
Jordan Accinelli <jordan.accinelli@threebyone.com.au>
2022-03-23 18:53:52 +1000

    Changed auth0 support to custom scope

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,16 +1,15 @@
-import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin, initAuth0 } from "@auth0/nextjs-auth0";
 
-export default handleAuth({
-    async login(req, res) {
-        try {
-            await handleLogin(req, res, {
-                authorizationParams: {
-                    audience: `${process.env.AUTH0_ISSUER_BASE_URL}/api/v2/`,
-                    scope: process.env.AUTH0_SCOPE,
-                },
-            });
-        } catch (error) {
-            res.status(error.status || 400).end(error.message);
-        }
+const auth = initAuth0({
+    clientID: process.env.AUTH0_CLIENT_ID,
+    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+    baseURL: process.env.AUTH0_BASE_URL,
+    secret: process.env.AUTH0_SECRET,
+    clientSecret: process.env.AUTH0_SECRET,
+    authorizationParams: {
+        response_type: "code",
+        scope: "openid profile email",
     },
 });
+
+export default auth.handleAuth();
